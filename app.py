d0d7320b5993e259cb0b442b11cb81f1b1e2375a
Vanshu <v.g>
2024-01-23 22:45:36 +0530

    same flow with gemini

--- app.py
+++ app.py
@@ -63,20 +63,12 @@ def improve_script(script):
     output = api_call(prompt=prompt)
     return output
 
-    # messages = [{"role": "user", "content": prompt}]
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=messages,
-    #     temperature=0,  # this is the degree of randomness of the model's output
-    # )
-    # return response.choices[0].message["content"]
-
-
 def generate_hooks(script, lang):
     prompt = f""" You will be provided with a script and you need to perform the following tasks:
 
-        - Provide 10 attention grabbing hook lines according to output script.
-        - Suggest 5 Captions, video shooting tips and video editing tips for output script.
+        - Provide 10 attention grabbing hook lines and 5 Captions according to output script.
+        - Suggest on how to create thumbnail according to output script.
+        - Suggest on how to shoot video and edit video according to output script.
         - Your output should be in {lang} language only.
 
         Script is delimited by triple backticks \
@@ -116,15 +108,12 @@ def save_feedback_to_csv(user_email, user_feedback, user_rating, user_preference
     st.success("Feedback saved successfully.")
 
 
-# Main Streamlit app for feedback form
 def feedbackform():
     # Get the file path for the logo
     logo_path = os.path.join("logo", "k.png")
     new_tab_title = 'Feedback Form'
     # Change the tab title
     st.set_page_config(page_title=new_tab_title, page_icon=logo_path, initial_sidebar_state="collapsed")
-    # Set page title
-    # st.set_page_config(page_title="Feedback Form", initial_sidebar_state="collapsed")
 
     # Set background color to black and text color to white
     st.markdown(
@@ -147,18 +136,6 @@ def main():
     new_tab_title = 'KontentGPT'
     # Change the tab title
     st.set_page_config(page_title=new_tab_title, page_icon=logo_path, layout="wide")
-    # google_analytics_script = """
-    #                     <!-- Google tag (gtag.js) -->
-    #                     <script async src="https://www.googletagmanager.com/gtag/js?id=G-N267LHMRTW"></script>
-    #                     <script>
-    #                       window.dataLayer = window.dataLayer || [];
-    #                       function gtag(){dataLayer.push(arguments);}
-    #                       gtag('js', new Date());
-    #
-    #                       gtag('config', 'G-N267LHMRTW');
-    #                     </script>
-    #                     """
-    # st.markdown(google_analytics_script, unsafe_allow_html=True)
 
     logo_style = """
            <style>
@@ -171,7 +148,7 @@ def main():
 
     # Display the styled logo
     st.markdown(logo_style, unsafe_allow_html=True)
-    st.image("logo/brand_logo2.svg", caption='Beta', use_column_width=False, output_format='auto', width=200)
+    st.image("logo/brand_logo2.svg", caption='Beta [Experiment]', use_column_width=False, output_format='auto', width=200)
     # tracker
     with streamlit_analytics.track():
         selected = option_menu(
@@ -191,7 +168,7 @@ def main():
             script_input = st.text_area("Drop Your Script Below",
                                         placeholder="Get Human Touch and Human Engaging Script")
             checkbox_state = st.checkbox(
-                "Tick me! If you want to Generate Hooks, Captions, Video Shooting Tips and Video Editing Tips as per your Script.")
+                "Tick me! If you want to Generate Hooks, Captions, Thumbnail Tips, Video Shooting Tips and Video Editing Tips as per your Script.")
             button_style = """
                     <style>
                         button {
@@ -302,7 +279,7 @@ def main():
                         st.markdown("<hr>", unsafe_allow_html=True)
                         if checkbox_state:
                             # Call the function when the checkbox is ticked
-                            st.write("Generating Hooks, Please wait.")
+                            st.success("Generating Hooks...")
                             result = generate_hooks(improved_script, input_script_lang)
                             st.write(
                                 "You can use the below hooks in the first line or in any other line of the script as per your requirement.")
@@ -316,7 +293,7 @@ def main():
                         st.markdown("<hr>", unsafe_allow_html=True)
                         if checkbox_state:
                             # Call the function when the checkbox is ticked
-                            st.write("Generating Hooks")
+                            st.success("Generating Hooks...")
                             result = generate_hooks(output_script, input_script_lang)
                             st.write(
                                 "You can use the below hooks in the first line or in any other line of the script as per your requirement.")
@@ -366,8 +343,8 @@ def main():
             st.markdown(
                 """
                 <div style="background-color: #0E1117; padding: 10px; text-align: center;">
-                    <p style="margin: 0;"><b>Contact us</b> - <a style="margin: 0;text-decoration: none;" href="mailto:teamalgo.ai@gmail.com">
-                        teamalgo.ai@gmail.com </a>
+                    <p style="margin: 0;"><b>Contact us</b> - <a style="margin: 0;text-decoration: none;" href="mailto:hello@kontentgpt.com">
+                        hello@kontentgpt.com </a>
                         |
                          <a style="margin: 0;text-decoration: none;" href="https://www.linkedin.com/company/algo-ai01/">
                             LinkedIn profile
@@ -404,194 +381,21 @@ def main():
             st.markdown(
                 """
                 <div style="background-color: #0E1117; padding: 2px; text-align: center;">
-                    <p style="margin: 10;"><b>Tip of the day is : </b>We Provide AI LLMs that help content creators to make perfect content.
-                          </p>
+                    <p style="margin: 10;"><b>Tip of the day is : </b>Follow the 120% Rule to seamlessly connect your first and last lines, creating an endless and seamless video.
+                          </p><br>
+                          <b>For example</b>: "Iss women ne is saal gautam adani aaur mukesh ambani se bhi jyada paise kamaye hai.<br>
+                          ...<br>
+                          ...<br>
+                          aaur iski vajah se aaj savitri jindal, india ki richest woman hai, jinki net worth 25.3 billion dollars ki hai. Isi tarah"
+                          <br><br>
+                          In the above example, last line is connected to the first line and user will rewatch your video without realising they are rewatching your video.
+                          
                 </div>
                 """,
                 unsafe_allow_html=True
             )
-            # st.write("Tip of the day is : ", tip)
 
-        # # Title
-        # st.title("Improve the Quality of your script with your AI Assistant.")
-        #
-        # # Step 1: Text Area for script input
-        # script_input = st.text_area("Drop Your Script Below", placeholder="Get Human Touch and Human Engaging Script")
-        # checkbox_state = st.checkbox(
-        #     "Tick me! If you want to Generate Hooks, Captions, Video Shooting tips and Video Editing Tips as per your script.")
-        # button_style = """
-        #     <style>
-        #         button {
-        #       position: relative;
-        #       margin: 0;
-        #       padding: 0.8em 1em;
-        #       outline: none;
-        #       text-decoration: none;
-        #       display: flex;
-        #       justify-content: center;
-        #       align-items: center;
-        #       cursor: pointer;
-        #       border: none;
-        #       text-transform: uppercase;
-        #       background-color: #333;
-        #       border-radius: 10px;
-        #       color: #fff;
-        #       font-weight: 300;
-        #       font-size: 18px;
-        #       font-family: inherit;
-        #       z-index: 0;
-        #       overflow: hidden;
-        #       transition: all 0.3s cubic-bezier(0.02, 0.01, 0.47, 1);
-        #     }
-        #
-        #     button:hover {
-        #       animation: sh0 0.5s ease-in-out both;
-        #     }
-        #
-        #     @keyframes sh0 {
-        #       0% {
-        #         transform: rotate(0deg) translate3d(0, 0, 0);
-        #       }
-        #
-        #       25% {
-        #         transform: rotate(7deg) translate3d(0, 0, 0);
-        #       }
-        #
-        #       50% {
-        #         transform: rotate(-7deg) translate3d(0, 0, 0);
-        #       }
-        #
-        #       75% {
-        #         transform: rotate(1deg) translate3d(0, 0, 0);
-        #       }
-        #
-        #       100% {
-        #         transform: rotate(0deg) translate3d(0, 0, 0);
-        #       }
-        #     }
-        #
-        #     button:hover span {
-        #       animation: storm 0.7s ease-in-out both;
-        #       animation-delay: 0.06s;
-        #     }
-        #
-        #     button::before,
-        #     button::after {
-        #       content: '';
-        #       position: absolute;
-        #       right: 0;
-        #       bottom: 0;
-        #       width: 100px;
-        #       height: 100px;
-        #       border-radius: 50%;
-        #       background: #fff;
-        #       opacity: 0;
-        #       transition: transform 0.15s cubic-bezier(0.02, 0.01, 0.47, 1), opacity 0.15s cubic-bezier(0.02, 0.01, 0.47, 1);
-        #       z-index: -1;
-        #       transform: translate(100%, -25%) translate3d(0, 0, 0);
-        #     }
-        #
-        #     button:hover::before,
-        #     button:hover::after {
-        #       opacity: 0.15;
-        #       transition: transform 0.2s cubic-bezier(0.02, 0.01, 0.47, 1), opacity 0.2s cubic-bezier(0.02, 0.01, 0.47, 1);
-        #     }
-        #
-        #     button:hover::before {
-        #       transform: translate3d(50%, 0, 0) scale(0.9);
-        #     }
-        #
-        #     button:hover::after {
-        #       transform: translate(50%, 0) scale(1.1);
-        #     }
-        # </style>
-        #
-        #         """
-        #
-        # st.markdown(button_style, unsafe_allow_html=True)
-        #
-        # # Step 2: Submit Button
-        # if st.button("Submit", key="submit_button"):
-        #     # Step 3: Perform tasks on submit
-        #     if not script_input:
-        #         st.warning("Please enter a script before submitting.")
-        #     else:
-        #         st.text("Please wait, Great results take time to appear.")
-        #
-        #         # Step 3.1: Improve the script
-        #         # improved_script = improve_script(script_input,script_type, script_language)
-        #         input_script_lang = input_script_language(script_input)
-        #         improved_script = improve_script(script_input)
-        #
-        #         if input_script_lang == "English":
-        #             st.success("Improved Script:")
-        #             st.write(improved_script)
-        #             st.markdown("<hr>", unsafe_allow_html=True)
-        #             if checkbox_state:
-        #                 # Call the function when the checkbox is ticked
-        #                 st.write("Generating Hooks, Please wait.")
-        #                 result = generate_hooks(improved_script, input_script_lang)
-        #                 st.write(
-        #                     "You can use the below hooks in the first line or in any other line of the script as per your requirement.")
-        #                 st.write(result)
-        #                 st.markdown("<hr>", unsafe_allow_html=True)
-        #
-        #         if not input_script_lang == "English":
-        #             output_script = translate_output_language(improved_script, input_script_lang)
-        #             st.success("Improved Script:")
-        #             st.write(output_script)
-        #             st.markdown("<hr>", unsafe_allow_html=True)
-        #             if checkbox_state:
-        #                 # Call the function when the checkbox is ticked
-        #                 st.write("Generating Hooks")
-        #                 result = generate_hooks(output_script, input_script_lang)
-        #                 st.write(
-        #                     "You can use the below hooks in the first line or in any other line of the script as per your requirement.")
-        #                 st.write(result)
-        #                 st.markdown("<hr>", unsafe_allow_html=True)
-
-        # Call feedbackForm.py
-        # feedbackform()
-        # subprocess.Popen(["streamlit", "run", "feedbackForm.py"])
-
-        # Footer Section
-        # st.markdown("<hr>", unsafe_allow_html=True)
-
-        # # Contact us
-        # st.markdown(
-        #     """
-        #     <div style="background-color: #f4f4f4; padding: 10px; text-align: center;">
-        #         <p style="margin: 0;"><b>Contact us</b> - <a style="margin: 0;text-decoration: none;" href="mailto:teamalgo.ai@gmail.com">
-        #             teamalgo.ai@gmail.com </a>
-        #             |
-        #              <a style="margin: 0;text-decoration: none;" href="https://www.linkedin.com/company/algo-ai01/">
-        #                 LinkedIn profile
-        #             </a>
-        #               </p>
-        #     </div>
-        #     """,
-        #     unsafe_allow_html=True
-        # )
-        # # About us
-        # st.markdown(
-        #     """
-        #     <div style="background-color: #f4f4f4; padding: 2px; text-align: center;">
-        #         <p style="margin: 10;"><b>About</b> -We Provide AI LLMs that help content creators to make perfect content.
-        #               </p>
-        #     </div>
-        #     """,
-        #     unsafe_allow_html=True
-        # )
-        # st.markdown("<hr>", unsafe_allow_html=True)
-        # st.markdown(
-        #     """
-        #        <div style="background-color: #f4f4f4; padding: 2px; text-align: center;">
-        #            <p style="margin: 10;"> We are continuously working to launch our final product as soon as possible. Our final product will provide end to end soltuions to land a perfect content. Through the utilization of our final product, content creators will experience seamless automation, making their work not only efficient but also effortlessly streamlined. </p>
-        #        </div>
-        #        """,
-        #     unsafe_allow_html=True
-        # )
-        # Footer
+  # Footer
 
     st.markdown(
         """
