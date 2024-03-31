a94e9845f7214064ecdd6c02960b7a7d2e52406e
Bzsmrf <dozerzzzz1234@gmail.com>
2024-03-31 19:59:04 +0530

    fix

--- Frontend/frontend-kgpt/src/components/ChatBar.tsx
+++ Frontend/frontend-kgpt/src/components/ChatBar.tsx
@@ -17,7 +17,7 @@ const ChatBar: React.FC<ChatBarProps> = ({ email }) => {
 	const [requestValue, setRequestValue] = useState<number | null>(null);
 	const [isChatStarted, setIsChatStarted] = useState<boolean | null>(false);
 
-	const isMobile = window.innerWidth <= 768;
+	// const isMobile = window.innerWidth <= 768;
 
 	const handleRadioClick = async (value: number) => {
 		setSelectedRadio(value);
@@ -136,7 +136,7 @@ const ChatBar: React.FC<ChatBarProps> = ({ email }) => {
 						onClick={handleSubmit}
 						title='Submit'
 					>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
 							<path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
 						</svg>
 
