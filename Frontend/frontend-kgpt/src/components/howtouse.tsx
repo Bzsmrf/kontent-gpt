e59c23b5d43e3b564cc679da9cbb488c7f040322
Bzsmrf <dozerzzzz1234@gmail.com>
2024-04-26 16:01:00 +0530

    added font

--- Frontend/frontend-kgpt/src/components/howtouse.tsx
+++ Frontend/frontend-kgpt/src/components/howtouse.tsx
@@ -6,7 +6,7 @@ const isMobile = window.innerWidth <= 768;
 const HowToUse = () => {
     return (
         <div className="how-to-use-container">
-            <h1>How to use</h1>
+            <h1 style={{ fontFamily: 'Roboto', fontSize: "24px" }}>How to use</h1>
             <div>
                 {isMobile ? <ReactPlayer
                     url="https://youtu.be/tGSBVagQdeM"
